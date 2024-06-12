2fb32bcb2351a081453bdc38d680018b0dda83e4
guilhermeriosdev <98851758+guilhermeriosdev@users.noreply.github.com>
2024-06-12 14:10:47 -0300

    Update conexao.php

--- Desenvolvimento/root/HeartStudio/conexao.php
+++ Desenvolvimento/root/HeartStudio/conexao.php
@@ -1,9 +1,15 @@
-<?php 
-$servidor="127.0.0.1";
-$usuario="root";
-$senha="usbw";
-$banco="agendamento";
-$conecta_db=mysql_connect($servidor, $usuario, $senha) or die (mysql_error());
-mysql_select_db($banco) or die ("Erro ao Conectar!");
-?>
+<?php
+$host = 'dpg-cpkt9ovsc6pc73f4blbg-a';
+$dbname = 'TattooStudioBD';
+$user = 'tattoostudiobd_user';
+$password = '8qfTsXmQh2wKvx5DEJMREg68ZpLMM2Av';
+$port = '5432';
+
+try {
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+    $pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    echo "Connected to the database successfully!";
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
 
