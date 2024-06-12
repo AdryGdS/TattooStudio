f32fa5dfeb2bf58b23ce4ace705bfac9f79a668e
guilhermeriosdev <98851758+guilhermeriosdev@users.noreply.github.com>
2024-06-12 15:01:50 -0300

    Update login.php

--- Desenvolvimento/root/HeartStudio/login.php
+++ Desenvolvimento/root/HeartStudio/login.php
@@ -1,36 +1,34 @@
-<?php 
-// Inclua o arquivo de conexão
-include 'conexao.php';
+<?php
+// Configuração da conexão com o banco de dados
+$db_url = "postgres://tattoostudiobd_user:8qfTsXmQh2wKvx5DEJMREg68ZpLMM2Av@dpg-cpkt9ovsc6pc73f4blbg-a/tattoostudiobd";
 
-// Receba os dados do formulário via POST
-$user = $_POST["txt_login"];
-$password = $_POST["txt_senha"];
+try {
+    // Conectando ao banco de dados usando a URL fornecida pelo Render
+    $pdo = new PDO($db_url);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexão com o banco de dados estabelecida com sucesso!<br>";
 
-echo "Usuário: " . $user . "<br>";
-echo "Senha: " . $password . "<br>";
+    // Receba os dados do formulário via POST
+    $user = $_POST["txt_login"];
+    $password = $_POST["txt_senha"];
 
-// Prepare e execute a consulta SQL
-$sql = $pdo->prepare("SELECT * FROM tb_login WHERE user = :user AND password = :password");
-$sql->bindParam(':user', $user);
-$sql->bindParam(':password', $password);
-$sql->execute();
+    // Prepare e execute a consulta SQL
+    $sql = $pdo->prepare("SELECT * FROM tb_login WHERE user = :user AND password = :password");
+    $sql->bindParam(':user', $user);
+    $sql->bindParam(':password', $password);
+    $sql->execute();
 
-// Verifique se o login foi bem-sucedido
-if ($sql->rowCount() > 0) {
-    echo "Conta logada com sucesso!<br>";
-
-    // Redirecione com base no tipo de usuário
-    $row = $sql->fetch(PDO::FETCH_ASSOC);
-    if (substr($row['user'], 0, 1) == 'f' && substr($row['user'], -1) == '@') {
-        // Usuário com user que começa com 'f' e termina com '@'
-        header("Location: homefunc.html"); // Redireciona para a página de funcionários
+    // Verifique se o login foi bem-sucedido
+    if ($sql->rowCount() > 0) {
+        echo "Conta logada com sucesso!<br>";
+        // Redirecionar o usuário após o login
+        header("Location: pagina_de_inicio.php");
         exit();
-    } else {
-        // Outros usuários
-        header("Location: perfil.php"); // Redireciona para a página de perfil
-        exit();
+    } else { 
+        echo "Usuário ou senha inválidos!<br>";  
     }
-} else { 
-    echo "Conta não logada!<br>";  
+} catch (PDOException $e) {
+    // Se ocorrer um erro ao conectar-se ao banco de dados
+    echo "Erro ao conectar-se ao banco de dados: " . $e->getMessage();
 }
 ?>
