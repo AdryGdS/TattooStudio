3d37634afa43713d9d5d41d9bd10389778ef3981
guilhermeriosdev <98851758+guilhermeriosdev@users.noreply.github.com>
2024-06-12 14:41:44 -0300

    Update login.php

--- Desenvolvimento/root/HeartStudio/login.php
+++ Desenvolvimento/root/HeartStudio/login.php
@@ -1,17 +1,27 @@
 <?php 
-Include 'conexao.php';
-$user = $_POST ["txt_login"];
-$password = $_POST ["txt_senha"];
-$sql = mysql_query ("select * from tb_login where
-                     (user='$user') and password='$password'");
-if (mysql_num_rows($sql) > 0) {
+// Inclua o arquivo de conexão
+include 'conexao.php';
+
+// Receba os dados do formulário via POST
+$user = $_POST["txt_login"];
+$password = $_POST["txt_senha"];
+
+// Prepare e execute a consulta SQL
+$sql = $pdo->prepare("SELECT * FROM tb_login WHERE user = :user AND password = :password");
+$sql->bindParam(':user', $user);
+$sql->bindParam(':password', $password);
+$sql->execute();
+
+// Verifique se o login foi bem-sucedido
+if ($sql->rowCount() > 0) {
     echo "<center>";
 	echo "<hr>";
 	echo "Conta logada com sucesso!";
 	echo "<hr>";
 	echo "<br>";
 
-    $row = mysql_fetch_assoc($sql);
+    // Redirecione com base no tipo de usuário
+    $row = $sql->fetch(PDO::FETCH_ASSOC);
     if (substr($row['user'], 0, 1) == 'f' && substr($row['user'], -1) == '@') {
         // Usuário com user que começa com 'f' e termina com '@'
         header("Location: homefunc.html"); // Redireciona para a página de funcionários
@@ -21,8 +31,7 @@ if (mysql_num_rows($sql) > 0) {
         header("Location: perfil.php"); // Redireciona para a página de perfil
         exit();
     }
-}
-else { 
+} else { 
     echo "<center>";
 	echo "<hr>";
 	echo "Conta não logada!";
